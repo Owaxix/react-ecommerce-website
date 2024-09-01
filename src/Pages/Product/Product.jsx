bec007692bc1e207bb300cfcd2ae19e02307fac9
Owaxix <owaxix1@gmail.com>
2024-09-02 01:35:24 +0500

    Added Related Product Functionality

--- src/Pages/Product/Product.jsx
+++ src/Pages/Product/Product.jsx
@@ -4,6 +4,7 @@ import { ShopContext } from '../../Context/ShopContext';
 import Breadcrums from '../../Components/Breadcrums/Breadcrums';
 import ProductDisplay from '../../Components/ProductDisplay/ProductDisplay';
 import DescriptionBox from '../../Components/DescriptionBox/DescriptionBox';
+import RelatedProducts from '../../Components/RelatedProducts/RelatedProducts';
 
 
 export const Product = () => {
@@ -15,6 +16,7 @@ export const Product = () => {
       <Breadcrums product={product}/>
       <ProductDisplay product={product}/>
       <DescriptionBox/>
+      <RelatedProducts/>
     </div>
   )
 }
